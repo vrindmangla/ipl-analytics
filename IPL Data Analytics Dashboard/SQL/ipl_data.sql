e05a9dd91d51a286f6fe2a74cba154438a9b9968
vrindmangla <vrindmangla05@gmail.com>
2026-07-02 00:07:42 +0530

    Add files via upload

---
+++ IPL Data Analytics Dashboard/SQL/ipl_data.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS squads;
+CREATE TABLE squads (
+    team_no INTEGER,
+    team_name TEXT,
+    player TEXT,
+    nationality TEXT,
+    "role" TEXT,
+    designation TEXT
+);
---
+++ IPL Data Analytics Dashboard/SQL/players_with_images.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS players_with_images;
+
+CREATE TABLE players_with_images (
+    player_name TEXT,
+    player_id INTEGER,
+    player TEXT,
+    prof_link TEXT,
+    img_link TEXT
+);
